ca2737b751bc1a7963b2d7e523385c679b73bb21
Buliyamba <148887446+Buliyamba@users.noreply.github.com>
2026-07-22 18:11:54 +0500

    Minor fix login_page.py, realization pages switching

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,14 +9,10 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        substring = self.browser.current_url.find("/login/")
-        assert substring is str, "This is not a login page"
+        assert self.browser.current_url.find("/login/"), "This is not a login page"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
         assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
         assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), "Register form is not presented"
