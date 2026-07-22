8fdfe9fd0dff5fe85ba90c3c6caaeb979ea56a61
Buliyamba <148887446+Buliyamba@users.noreply.github.com>
2026-07-22 05:32:27 +0500

    Update main_page.py with new function. Rebuild open function and init in base_page.py. Any page now will have their own PAGE_URL

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,25 @@
-class BasePage():
-    def __init__(self, browser, url):
+from selenium.common.exceptions import NoSuchElementException
+
+
+class BasePage:
+    def __init__(self, browser, url=None, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     def open(self):
-        self.browser.get(self.url)
+        url_to_open = None
+        if self.url:
+            url_to_open = self.url
+        elif hasattr(self, "PAGE_URL"):
+            url_to_open = self.PAGE_URL
+        else:
+            raise ValueError("There should be URL")
+        self.browser.get(url_to_open)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
